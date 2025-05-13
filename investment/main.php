86a8c95ebd320a79a67608db70c6aa0a0e50499f
derphilovic <philovic@outlook.de>
2025-05-13 16:55:34 +0200

    header (gurksino)

--- investment/main.php
+++ investment/main.php
@@ -1,4 +1,12 @@
 <style>
 <?php include 'cas-style.css'; ?>
 </style>
-<h1>GURKSINO!</h1>
+<section id="header">
+    <div>
+        <ul id="navbar">
+        <li><h1>GURKSINO!</h1></li>
+        <li><h2>GURKRONE: 129</h2></li>
+        <li><h2>ANMELDEN</h2></li>
+        </ul>
+    </div>
+</section>
